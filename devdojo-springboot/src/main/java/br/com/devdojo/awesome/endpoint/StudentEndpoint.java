e328a2c4c5d4ed53c7dc4d49cfd8cb8a164a02e4
jailcomfranssa <jail_son_0@hotmail.com>
2022-09-20 13:48:34 -0300

    usando anotações @Component, @AutoWired & @SpringBootApplication

--- devdojo-springboot/src/main/java/br/com/devdojo/awesome/endpoint/StudentEndpoint.java
+++ devdojo-springboot/src/main/java/br/com/devdojo/awesome/endpoint/StudentEndpoint.java
@@ -1,11 +1,14 @@
 package br.com.devdojo.awesome.endpoint;
 
 import br.com.devdojo.awesome.model.Student;
+import br.com.devdojo.awesome.util.DateUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 
@@ -13,8 +16,12 @@ import java.util.List;
 @RequestMapping("/student")
 public class StudentEndpoint {
 
+    @Autowired
+    private DateUtil dateUtil;
+
     @RequestMapping(method = RequestMethod.GET, path = "/list")
     public List<Student> listAll() {
+        System.out.println(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
         return Arrays.asList(new Student("Jailson"), new Student("Maria"), new Student("Jose"));
     }
 }
