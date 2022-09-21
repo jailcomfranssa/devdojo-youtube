268e7fcf315ec70a057cb22d54b2fc36a4924a1d
jailcomfranssa <jail_son_0@hotmail.com>
2022-09-21 13:06:13 -0300

    criando projeto usando MySQL

---
+++ devdojo/src/main/java/com/devdojo/essentials/DevdojoApplication.java
@@ -0,0 +1,13 @@
+package com.devdojo.essentials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevdojoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevdojoApplication.class, args);
+	}
+
+}
---
+++ devdojo/src/test/java/com/devdojo/essentials/DevdojoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devdojo.essentials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevdojoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
