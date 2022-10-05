39a206adf88fc2f0eda681aa8aa00f611b85bf77
jailcomfranssa <jail_son_0@hotmail.com>
2022-10-05 15:36:53 -0300

    criando Autenticação com CSRF

--- devdojo-essentials-02/src/main/java/br/com/devdojo/essentials/config/SecurityConfig.java
+++ devdojo-essentials-02/src/main/java/br/com/devdojo/essentials/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Log4j2
 @EnableWebSecurity
@@ -14,11 +15,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests()
+        http.csrf().disable()
+//                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+//                .and()
+                .authorizeHttpRequests()
                 .anyRequest()
-                        .authenticated()
-                                .and()
-                                        .httpBasic();
+                .authenticated()
+                .and()
+                .httpBasic();
 
     }
 
