ca690c81f936b804b0bceb8543869852049732c4
jailcomfranssa <jail_son_0@hotmail.com>
2022-10-07 13:02:45 -0300

    criando classe corsFilter

---
+++ devdojo-essentials-02/src/main/java/br/com/devdojo/essentials/config/CorsConfig.java
@@ -0,0 +1,34 @@
+package br.com.devdojo.essentials.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    @Primary
+    public CorsFilter corsFilter(){
+        final UrlBasedCorsConfigurationSource configurationSource = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("OPTIONS");
+        corsConfiguration.addAllowedMethod("HEAD");
+        corsConfiguration.addAllowedMethod("GET");
+        corsConfiguration.addAllowedMethod("PUT");
+        corsConfiguration.addAllowedMethod("POST");
+        corsConfiguration.addAllowedMethod("DELETE");
+        corsConfiguration.addAllowedMethod("PATCH");
+
+        configurationSource.registerCorsConfiguration("/**",corsConfiguration);
+
+        return new CorsFilter(configurationSource);
+    }
+}
